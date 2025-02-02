385c972cfb36221b668c396791ab3b0fe1ce5b1c
Abhishek <87256096+abhishek-maurya576@users.noreply.github.com>
2025-02-02 16:28:54 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,5 @@
 import sys
-
-
 def main():
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-
+    sys.stdout.write("$ ")
     # Wait for user input
     input()
-
-
-if __name__ == "__main__":
-    main()
